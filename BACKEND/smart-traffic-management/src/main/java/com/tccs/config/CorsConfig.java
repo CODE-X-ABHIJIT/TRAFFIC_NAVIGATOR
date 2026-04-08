85431f31afe368d6663256d332e2acd2146b3ba1
CODE-X-ABHIJIT <codexabhijit@gmail.com>
2026-04-08 21:22:16 +0530

    cors fix

--- BACKEND/smart-traffic-management/src/main/java/com/tccs/config/CorsConfig.java
+++ BACKEND/smart-traffic-management/src/main/java/com/tccs/config/CorsConfig.java
@@ -1,21 +1,24 @@
 package com.tccs.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
-
 @Configuration
 public class CorsConfig {
-
+	@Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200", "*"));
+        //config.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200", "*"));
+        config.setAllowedOriginPatterns(Arrays.asList(allowedOrigins));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
