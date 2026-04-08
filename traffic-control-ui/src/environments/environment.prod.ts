17354e24ddea43117bd16e852464bd26ad06355d
CODE-X-ABHIJIT <codexabhijit@gmail.com>
2026-04-08 22:08:25 +0530

    env file fix 2

---
+++ traffic-control-ui/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://traffic-navigator-backend.onrender.com/api',
+  wsBaseUrl: 'wss://traffic-navigator-backend.onrender.com/ws-traffic/websocket'
+};
---
+++ traffic-control-ui/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:8090/api',
+  wsBaseUrl: 'ws://localhost:8090/ws-traffic/websocket'
+};
