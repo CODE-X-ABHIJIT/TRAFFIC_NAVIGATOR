57bf9c2d8fe4a88bef8509e80f20b5f593d1adc4
CODE-X-ABHIJIT <codexabhijit@gmail.com>
2026-04-08 22:58:53 +0530

    cors fix 1

--- BACKEND/smart-traffic-management/src/main/java/com/tccs/config/CorsConfig.java
+++ BACKEND/smart-traffic-management/src/main/java/com/tccs/config/CorsConfig.java
@@ -1,24 +1,21 @@
 package com.tccs.config;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
-	@Value("${app.cors.allowed-origins}")
-    private String[] allowedOrigins;
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        //config.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200", "*"));
-        config.setAllowedOriginPatterns(Arrays.asList(allowedOrigins));
+        config.setAllowedOriginPatterns(Arrays.asList("https://traffic-navigator.onrender.com", "*"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 
